fb0703d44a243c04724536fcd720e99475d35bf1
luiS2 <87776084+lluuiizz@users.noreply.github.com>
2022-08-19 10:55:39 +0000

    input box added and other things

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+function submitAnswear(element) {
+
+  alert(element.value)
+  element.value = ""
+  answear = element.value
+
+}
+
+function keyPress(e) {
+  var keynum;
+
+  if (window.event) { // IE                  
+    keynum = e.keyCode;
+  } else if (e.which) { // Netscape/Firefox/Opera                 
+    keynum = e.which;
+  }
+
+  if (keynum == 13) {   // 13 == enter
+    let element = document.getElementById('field');
+    submitAnswear(element)
+  }
+}
 
