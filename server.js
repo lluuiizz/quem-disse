e518501f4c4908d99602b9486526163b3d243470
Luiz Guilherme Costa da Silva <luiz.developer.acc@gmail.com>
2022-08-27 16:31:58 -0300

    modified client object with more abrangent things such the phraseObject itself

--- server.js
+++ server.js
@@ -22,28 +22,35 @@ function directoryFiles(dirPath) {
 	return fs.readdirSync(dirPath)
 }
 
-function getSomeRandomPhraseInCategorie(categorie) {
-	let categoriePath = path.join(__dirname, 'phrases/'+categorie+'/')
-	let phraseIndex = pickRandomIndex(directoryFiles(categoriePath).length, 1)
-	const contents = fs.readFileSync(categoriePath + phraseIndex.toString() + '.txt', 'utf-8').split('\n')
+function getSomePhraseByCategory(category) {
+	let categoryPath = path.join(__dirname, 'phrases/'+category+'/')
+	let phraseIndex = pickRandomIndex(directoryFiles(categoryPath).length, 1)
+	const contents = fs.readFileSync(categoryPath + phraseIndex.toString() + '.txt', 'utf-8').split('\n')
 	const length = contents.length
 	return {
 		phrase : contents.slice(0, length - 2),
 		correctAnswear: contents[length - 2],
 		index: phraseIndex,
-		categorie: categorie
 	}
 }
 
-function getSomeCategorie() {
-	let categorie = categoriesOfPhrases[pickRandomIndex(amountOfCategories, 0)]
-	let amount = directoryFiles(path.join(__dirname, 'phrases/'+categorie+'/')).length
+function getSomeCategory() {
+	let category = categorysOfPhrases[pickRandomIndex(amountOfCategorys, 0)]
+	let amount = directoryFiles(path.join(__dirname, 'phrases/'+category+'/')).length
 	return {
-		categorie: categorie,
-		amountInCategorie: amount 
+		category: category,
+		amountInCategory: amount
 	}
 }
 
+
+function getPhraseObject() {
+		let category = getSomeCategory()
+		let phrase = getSomePhraseByCategory(category.category)
+
+		return {...category, ...phrase}
+
+}
 function pickRandomIndex(range, initValue) {
 	return generated = Math.floor(Math.random() * range + initValue)
 }
@@ -61,7 +68,8 @@ const client = (id) => {
     return {
         id : id,
         lifePoints : 5,
-		correctAnswears: 0
+				correctAnswears: 0,
+				currentPhrase : getPhraseObject()
     }
 }
 /////////////////////////////////////////////////////////////
@@ -72,39 +80,46 @@ http.listen(3000, function() {
 });
 /////////////////////////////////////////////////////////////
 
-//	Constants of categorie
-const categoriesOfPhrases = directoryFiles(path.join(__dirname, 'phrases'))	//	Count the amount of categories in phrases directory
-const amountOfCategories = categoriesOfPhrases.length
-console.log(categoriesOfPhrases)
+//	Constants of category
+const categorysOfPhrases = directoryFiles(path.join(__dirname, 'phrases'))	//	Count the amount of categorys in phrases directory
+const amountOfCategorys = categorysOfPhrases.length
+console.log(categorysOfPhrases)
 /////////////////////////////////////////////////////////////
 
 
 io.on('connection', socket => {
 	console.log("New connection", socket.id)
 
-	let categorieChoosed = getSomeCategorie()
-	let phraseObject = getSomeRandomPhraseInCategorie(categorieChoosed.categorie)
+		//	Instantiates an new element in the lifePoints array and emit the first phrase
+	clients.push(client(socket.id))
+	newClientIndex = clients.length - 1
 
-	socket.on('answear', (answear, indexesAlreadyUsed) => {
-		var hasSameValue = answear == phraseObject.correctAnswear
+  socket.emit("initLifePoints", 5)
+	socket.emit("phrase", clients[newClientIndex].currentPhrase.phrase, clients[newClientIndex].currentPhrase.index);
+
+	console.log("\n\nClient Object: ", clients[newClientIndex])
+
+	socket.on('answear', (answear, indexesAlreadyUsed, sockete) => {
+		console.log(`\n\nNew answear: ${answear}\nIndexesUsed: ${indexesAlreadyUsed}\nClient id: ${sockete}`)
+	/*	var hasSameValue = answear == phraseObject.correctAnswear
 		console.log(phraseObject)
 
 		if (hasSameValue) {
 
-			if (clients[findClientInClientsList(socket.id)].correctAnswears == 50) {
+			if (clients[findClientInClientsList(id)].correctAnswears == 50) {
 			socket.emit("win")
 			}
 
-			else if (categorieChoosed.amountInCategorie > indexesAlreadyUsed.length && indexesAlreadyUsed.length <= 10) {
+			else if (categoryChoosed.amountInCategory > indexesAlreadyUsed.length && indexesAlreadyUsed.length <= 10) {
 				while (indexesAlreadyUsed.indexOf(phraseObject.index) != -1) {
-					phraseObject = getSomeRandomPhraseInCategorie(categorieChoosed.categorie)
+					phraseObject = getSomeRandomPhraseInCategory(categoryChoosed.category)
 				}
-				clients[findClientInClientsList(socket.id)].correctAnswears += 1
+				clients[findClientInClientsList(id)].correctAnswears += 1
 				socket.emit("phrase", phraseObject.phrase, phraseObject.index)	//	Emit next phrase
 			}
 			else {
-				categorieChoosed = getSomeCategorie()
-				phraseObject = getSomeRandomPhraseInCategorie(categorieChoosed.categorie)
+				categoryChoosed = getSomeCategory()
+				phraseObject = getSomeRandomPhraseInCategory(categoryChoosed.category)
 				socket.emit("clearIndexArray")
 				socket.emit("phrase", phraseObject.phrase, phraseObject.index)	//	Emit next phrase
 			}
@@ -112,14 +127,13 @@ io.on('connection', socket => {
 		}
 		else {
 			//	Get the index in the array of the id received, decrease lifePoints and emit the event
-			let indexIdInArray = findClientInClientsList(socket.id)
+			let indexIdInArray = findClientInClientsList(id)
 			clients[indexIdInArray].lifePoints -= 1
 			socket.emit("changeLifePoints", clients[indexIdInArray].lifePoints)
 			if (clients[indexIdInArray].lifePoints == 0) {socket.emit("lose")}
-		}
+		}*/
 	})
-		//	Instantiates an new element in the lifePoints array and emit the first phrase
-	clients.push(client(socket.id))
-    socket.emit("initLifePoints", 5)
-	socket.emit("phrase", phraseObject.phrase, phraseObject.index);
 })
+
+
+
